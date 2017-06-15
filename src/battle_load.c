034618feaefbdbd2bb62146b79fce70915cbab61
NewDenverCity <NewDenverCity@users.noreply.github.com>
2017-06-15 10:44:59 -0400

    Create battle_load.c

--- src/battle_load.c
+++ src/battle_load.c
@@ -3,18 +3,20 @@
 
 #define FLAG 0x230
 
+extern const struct CustomTrainerPokemon EnemyTeam =
+{
+	{SPECIES_CHARMELEON, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_LONELY, 0, 0, 0, 0, 0, 0},
+	{SPECIES_CHARMELEON, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_LONELY, 0, 0, 0, 0, 0, 0}
+};
+
 void battle_load_hack(u16 trainerid) {
+	u32 generated_pid;
 	if ((flag_check(FLAG)) == true) {
 		flag_set(FLAG);
 		for (u8 i = 0; i < 6; i++) {
 			do {
-				u32 generated_pid = rand() | (rand << 0x10);
-			} while (EnemyTeam->nature = (get_nature_from_pid(generated_pid)));
+				generated_pid = rand() | (rand() << 0x10);
+			} while (EnemyTeam[i]->nature = (get_nature_from_pid(generated_pid)));
 		}
 	}
 }
-
-const struct CustomTrainerPokemon EnemyTeam =
-{
-	SPECIES_CHARMELEON, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_LONELY, 0, 0, 0, 0, 0, 0
-};
