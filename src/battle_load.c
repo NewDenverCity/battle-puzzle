8f0772bdf7b72cfa2af741b0e7f0e64cdafeb843
NewDenverCity <NewDenverCity@users.noreply.github.com>
2017-06-15 14:43:13 -0400

    Improved the struct, added pokemon_make_full

--- src/battle_load.c
+++ src/battle_load.c
@@ -1,12 +1,16 @@
-#include <pokeagb/pokeagb.h>
+include <pokeagb/pokeagb.h>
 #include "battle_load.h"
 
 #define FLAG 0x230
 
-extern const struct CustomTrainerPokemon EnemyTeam =
+extern const struct CustomTrainerPokemon EnemyTeam[6] =
 {
-	{SPECIES_CHARMELEON, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_LONELY, 0, 0, 0, 0, 0, 0},
-	{SPECIES_CHARMELEON, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_LONELY, 0, 0, 0, 0, 0, 0}
+	{SPECIES_BULBASAUR, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_HARDY, 0, 0, 0, 0, 0, 0},
+	{SPECIES_IVYSAUR, 16, 1, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_LONELY, 0, 0, 0, 0, 0, 0},
+	{SPECIES_VENUSAUR, 32, 2, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_BRAVE, 0, 0, 0, 0, 0, 0},
+	{SPECIES_CHARMANDER, 8, 3, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_ADAMANT, 0, 0, 0, 0, 0, 0},
+	{SPECIES_CHARMELEON, 16, 4, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_NAUGHTY, 0, 0, 0, 0, 0, 0},
+	{SPECIES_CHARIZARD, 32, 5, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_BOLD, 0, 0, 0, 0, 0, 0}
 };
 
 void battle_load_hack(u16 trainerid) {
@@ -16,7 +20,8 @@ void battle_load_hack(u16 trainerid) {
 		for (u8 i = 0; i < 6; i++) {
 			do {
 				generated_pid = rand() | (rand() << 0x10);
-			} while (EnemyTeam[i]->nature = (get_nature_from_pid(generated_pid)));
+			} while (EnemyTeam[i].nature == (get_nature_from_pid(generated_pid)));
+			pokemon_make_full(*party_opponent[i], EnemyTeam[i].species, EnemyTeam[i].level, EnemyTeam[i].ivs, true, generated_pid, 0, 0);
 		}
 	}
 }
