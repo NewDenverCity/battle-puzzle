1099daed75e4edfee6e4617c70f5e2fb38c52bbd
Denver Hopkins <Denver Hopkins>
2017-06-17 10:17:06 -0400

    Make arrays actually work.

--- src/battle_load.c
+++ src/battle_load.c
@@ -1,16 +1,16 @@
-include <pokeagb/pokeagb.h>
+#include <pokeagb/pokeagb.h>
 #include "battle_load.h"
 
 #define FLAG 0x230
 
-extern const struct CustomTrainerPokemon EnemyTeam[6] =
+const struct CustomTrainerPokemon EnemyTeam[6] =
 {
-	{SPECIES_BULBASAUR, 8, 0, [0], ITEM_NONE, NATURE_HARDY, 0, 0, 0, 0, 0, 0}, /* i changed the struct */
-	{SPECIES_IVYSAUR, 16, 1, 0, 0, 0, 0, ITEM_NONE, NATURE_LONELY, 0, 0, 0, 0, 0, 0},
-	{SPECIES_VENUSAUR, 32, 2, 0, 0, 0, 0, ITEM_NONE, NATURE_BRAVE, 0, 0, 0, 0, 0, 0},
-	{SPECIES_CHARMANDER, 8, 3, 0, 0, 0, 0, ITEM_NONE, NATURE_ADAMANT, 0, 0, 0, 0, 0, 0},
-	{SPECIES_CHARMELEON, 16, 4, 0, 0, 0, 0, ITEM_NONE, NATURE_NAUGHTY, 0, 0, 0, 0, 0, 0},
-	{SPECIES_CHARIZARD, 32, 5, 0, 0, 0, 0, ITEM_NONE, NATURE_BOLD, 0, 0, 0, 0, 0, 0}
+	{SPECIES_BULBASAUR, 8, 0, {0, 0, 0, 0}, ITEM_NONE, NATURE_HARDY, 0, 0, 0, 0, 0, 0}, /* i changed the struct */
+	{SPECIES_IVYSAUR, 16, 1, {0, 0, 0, 0}, ITEM_NONE, NATURE_LONELY, 0, 0, 0, 0, 0, 0},
+	{SPECIES_VENUSAUR, 32, 2, {0, 0, 0, 0}, ITEM_NONE, NATURE_BRAVE, 0, 0, 0, 0, 0, 0},
+	{SPECIES_CHARMANDER, 8, 3, {0, 0, 0, 0}, ITEM_NONE, NATURE_ADAMANT, 0, 0, 0, 0, 0, 0},
+	{SPECIES_CHARMELEON, 16, 4, {0, 0, 0, 0}, ITEM_NONE, NATURE_NAUGHTY, 0, 0, 0, 0, 0, 0},
+	{SPECIES_CHARIZARD, 32, 5, {0, 0, 0, 0}, ITEM_NONE, NATURE_BOLD, 0, 0, 0, 0, 0, 0}
 };
 
 void battle_load_hack(u16 trainerid) {
