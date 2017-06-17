e6f35a1d0465923f330a6d0aaaa56d5b2d1f8cd2
NewDenverCity <NewDenverCity@users.noreply.github.com>
2017-06-17 09:24:34 -0400

    Update battle_load.c

--- src/battle_load.c
+++ src/battle_load.c
@@ -5,12 +5,12 @@ include <pokeagb/pokeagb.h>
 
 extern const struct CustomTrainerPokemon EnemyTeam[6] =
 {
-	{SPECIES_BULBASAUR, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_HARDY, 0, 0, 0, 0, 0, 0},
-	{SPECIES_IVYSAUR, 16, 1, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_LONELY, 0, 0, 0, 0, 0, 0},
-	{SPECIES_VENUSAUR, 32, 2, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_BRAVE, 0, 0, 0, 0, 0, 0},
-	{SPECIES_CHARMANDER, 8, 3, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_ADAMANT, 0, 0, 0, 0, 0, 0},
-	{SPECIES_CHARMELEON, 16, 4, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_NAUGHTY, 0, 0, 0, 0, 0, 0},
-	{SPECIES_CHARIZARD, 32, 5, 0, 0, 0, 0, 0, 0, 0, 0, ITEM_NONE, NATURE_BOLD, 0, 0, 0, 0, 0, 0}
+	{SPECIES_BULBASAUR, 8, 0, [0], ITEM_NONE, NATURE_HARDY, 0, 0, 0, 0, 0, 0}, /* i changed the struct */
+	{SPECIES_IVYSAUR, 16, 1, 0, 0, 0, 0, ITEM_NONE, NATURE_LONELY, 0, 0, 0, 0, 0, 0},
+	{SPECIES_VENUSAUR, 32, 2, 0, 0, 0, 0, ITEM_NONE, NATURE_BRAVE, 0, 0, 0, 0, 0, 0},
+	{SPECIES_CHARMANDER, 8, 3, 0, 0, 0, 0, ITEM_NONE, NATURE_ADAMANT, 0, 0, 0, 0, 0, 0},
+	{SPECIES_CHARMELEON, 16, 4, 0, 0, 0, 0, ITEM_NONE, NATURE_NAUGHTY, 0, 0, 0, 0, 0, 0},
+	{SPECIES_CHARIZARD, 32, 5, 0, 0, 0, 0, ITEM_NONE, NATURE_BOLD, 0, 0, 0, 0, 0, 0}
 };
 
 void battle_load_hack(u16 trainerid) {
@@ -21,7 +21,12 @@ void battle_load_hack(u16 trainerid) {
 			do {
 				generated_pid = rand() | (rand() << 0x10);
 			} while (EnemyTeam[i].nature == (get_nature_from_pid(generated_pid)));
-			pokemon_make_full(*party_opponent[i], EnemyTeam[i].species, EnemyTeam[i].level, EnemyTeam[i].ivs, true, generated_pid, 0, 0);
+			pokemon_make_full(&party_opponent[i], EnemyTeam[i].species, EnemyTeam[i].level, EnemyTeam[i].ivs, true, generated_pid, 0, 0);
+			for (u8 j = 0; j < 4; j++) {
+				u16 move_thing = EnemyTeam[i].move[j];
+				pokemon_setattr(&party_opponent[i], (REQUEST_MOVE1 + j), &move_thing);
+			}
+			pokemon_restore_pp(&party_opponent[i]);
 		}
 	}
 }
