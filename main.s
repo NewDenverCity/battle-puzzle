a9ba00c68e1e5b9c2da41549b3e90648de55a0ef
NewDenverCity <NewDenverCity@users.noreply.github.com>
2017-06-19 10:20:26 -0400

    Update main.s

--- main.s
+++ main.s
@@ -1,6 +1,8 @@
         .gba
         .thumb
         .open "roms/BPRE0.gba","build/test.gba", 0x08000000
+        
+        .include "patches/bx_battle_load_hack.s"
 
        .org 0x0871A240
         .importobj "build/linked.o"
